dae9c8ee3f2a97937f90d08421c70c8c77f3543f
samyakjm <56508479+aksm123@users.noreply.github.com>
2020-01-16 16:54:32 +0530

    1st commit

---
+++ sth_sdp/quizzes/models.py
@@ -0,0 +1,216 @@
+import re
+import json
+from django.db import models
+from django.core.exceptions import ValidationError, ImproperlyConfigured
+from django.core.validators import MaxValueValidator, validate_comma_separated_integer_list
+from django.utils.timezone import now
+from django.conf import settings
+from django.utils.translation import ugettext as _
+from model_utils.managers import InheritanceManager
+from django.db.models.signals import pre_save, post_save
+import io
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
+class CategoryManager(models.Manager):
+
+    def new_category(self, category):
+        new_category = self.create(category=re.sub('\s+', '-', category)
+                                   .lower())
+
+        new_category.save()
+        return new_category
+
+
+class Category(models.Model):
+
+    category = models.CharField(
+        verbose_name=_("Category"),
+        max_length=250, blank=True,
+        unique=True, null=True)
+
+    objects = CategoryManager()
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.category
+
+
+class Quiz(models.Model):
+
+    title = models.CharField(
+        verbose_name=_("Title"),
+        max_length=60, blank=False)
+
+    description = models.TextField(
+        verbose_name=_("Description"),
+        blank=True, help_text=_("a description of the quiz"))
+
+    url = models.SlugField(
+        max_length=60, blank=False,
+        help_text=_("a user friendly url"),
+        verbose_name=_("user friendly url"))
+
+    category = models.ForeignKey(
+        Category, null=True, blank=True,
+        verbose_name=_("Category"), on_delete=models.CASCADE)
+
+    random_order = models.BooleanField(
+        blank=False, default=False,
+        verbose_name=_("Random Order"),
+        help_text=_("Display the questions in "
+                    "a random order or as they "
+                    "are set?"))
+
+    max_questions = models.PositiveIntegerField(
+        blank=True, null=True, verbose_name=_("Max Questions"),
+        help_text=_("Number of questions to be answered on each attempt."))
+
+    answers_at_end = models.BooleanField(
+        blank=False, default=False,
+        help_text=_("Correct answer is NOT shown after question."
+                    " Answers displayed at the end."),
+        verbose_name=_("Answers at end"))
+
+    exam_paper = models.BooleanField(
+        blank=False, default=False,
+        help_text=_("If yes, the result of each"
+                    " attempt by a user will be"
+                    " stored. Necessary for marking."),
+        verbose_name=_("Exam Paper"))
+
+    single_attempt = models.BooleanField(
+        blank=False, default=False,
+        help_text=_("If yes, only one attempt by"
+                    " a user will be permitted."
+                    " Non users cannot sit this exam."),
+        verbose_name=_("Single Attempt"))
+
+    pass_mark = models.SmallIntegerField(
+        blank=True, default=0,
+        verbose_name=_("Pass Mark"),
+        help_text=_("Percentage required to pass exam."),
+        validators=[MaxValueValidator(100)])
+
+    success_text = models.TextField(
+        blank=True, help_text=_("Displayed if user passes."),
+        verbose_name=_("Success Text"))
+
+    fail_text = models.TextField(
+        verbose_name=_("Fail Text"),
+        blank=True, help_text=_("Displayed if user fails."))
+
+    draft = models.BooleanField(
+        blank=True, default=False,
+        verbose_name=_("Draft"),
+        help_text=_("If yes, the quiz is not displayed"
+                    " in the quiz list and can only be"
+                    " taken by users who can edit"
+                    " quizzes."))
+
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        self.url = re.sub('\s+', '-', self.url).lower()
+
+        self.url = ''.join(letter for letter in self.url if
+                           letter.isalnum() or letter == '-')
+
+        if self.single_attempt is True:
+            self.exam_paper = True
+
+        if self.pass_mark > 100:
+            raise ValidationError('%s is above 100' % self.pass_mark)
+
+        super(Quiz, self).save(force_insert, force_update, *args, **kwargs)
+
+    class Meta:
+        verbose_name = _("Quiz")
+        verbose_name_plural = _("Quizzes")
+
+    def __str__(self):
+        return self.title
+
+    def get_questions(self):
+        return self.question_set.all().select_subclasses()
+
+    @property
+    def get_max_score(self):
+        return self.get_questions().count()
+
+    def anon_score_id(self):
+        return str(self.id) + "_score"
+
+    def anon_q_list(self):
+        return str(self.id) + "_q_list"
+
+    def anon_q_data(self):
+        return str(self.id) + "_data"
+
+
+
+class Question(models.Model):
+    """
+    Base class for all question types.
+    Shared properties placed here.
+    """
+
+    quiz = models.ManyToManyField(Quiz,
+                                  verbose_name=_("Quiz"),
+                                  blank=True)
+
+    category = models.ForeignKey(Category,
+                                 verbose_name=_("Category"),
+                                 blank=True,
+                                 null=True, on_delete=models.CASCADE)
+
+    figure = models.ImageField(upload_to='uploads/%Y/%m/%d',
+                               blank=True,
+                               null=True,
+                               verbose_name=_("Figure"))
+
+    content = models.CharField(max_length=1000,
+                               blank=False,
+                               help_text=_("Enter the question text that "
+                                           "you want displayed"),
+                               verbose_name=_('Question'))
+
+    explanation = models.TextField(max_length=2000,
+                                   blank=True,
+                                   help_text=_("Explanation to be shown "
+                                               "after the question has "
+                                               "been answered."),
+                                   verbose_name=_('Explanation'))
+
+    objects = InheritanceManager()
+
+    class Meta:
+        verbose_name = _("Question")
+        verbose_name_plural = _("Questions")
+        ordering = ['category']
+
+    def __str__(self):
+        return self.content
+
+
+
+
+
+   
+
+def create_user(data):
+    user =  User.objects.create_user(username=data['username'], 
+                            email=data['email'],
+                            password=data['password'],
+                            first_name=data['first_name'],
+                            last_name=data['last_name']
+                            )
+    user.is_admin=False
+    user.is_staff=False
+    user.save()
+
+
+
+
